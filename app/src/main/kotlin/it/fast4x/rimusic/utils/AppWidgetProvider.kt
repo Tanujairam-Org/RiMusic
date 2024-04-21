5da93edeecaaaf9fac96a055bfc51adc9d5baf7d
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-04-21 20:18:14 +0530

    Create AppWidgetProvider.kt

---
+++ app/src/main/kotlin/it/fast4x/rimusic/utils/AppWidgetProvider.kt
@@ -0,0 +1,8 @@
+// kugou/src/main/kotlin/it/fast4x/kugou/widgets/MyWidgetProvider.kt
+package it.fast4x.kugou.widgets
+
+import android.appwidget.AppWidgetProvider
+
+class MyWidgetProvider : AppWidgetProvider() {
+    // Implement your widget functionality here
+}
